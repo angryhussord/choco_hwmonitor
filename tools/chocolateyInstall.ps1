33a93abde78b334642d1762379dc69873b334a68
angryhussord <angryhussord@outlook.com>
2014-12-18 15:49:32 -0800

    Initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'hwmonitor'
+$installerType = 'EXE'
+$url = 'ftp://ftp.cpuid.com/hwmonitor/hwmonitor_1.26-setup.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
