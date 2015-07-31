67b7fd77ac227cfe16a2e072b1c4b09a2815e802
angryhussord <angryhussord@outlook.com>
2015-07-31 15:58:44 -0700

    updating for 1.28 release of hwmonitor

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,6 @@
-﻿$packageName = 'hwmonitor'
-$installerType = 'EXE'
-$url = 'ftp://ftp.cpuid.com/hwmonitor/hwmonitor_1.26-setup.exe'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+﻿$packageName = 'hwmonitor';
+$installerType = 'EXE';
+$url = 'http://download.cpuid.com/hwmonitor/hwmonitor_1.28.exe';
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-';
+$validExitCodes = @(0);
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes;
