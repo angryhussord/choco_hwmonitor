2d208e0d29f9db36914ec927acd793d40d454ecb
Patrick Hufford <pahuff@microsoft.com>
2016-11-08 09:46:38 -0800

    updating for 1.3 release

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,21 @@
-﻿$packageName = 'hwmonitor';
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'hwmonitor';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
 $installerType = 'EXE';
-$url = 'http://download.cpuid.com/hwmonitor/hwmonitor_1.29.exe';
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-';
-$validExitCodes = @(0);
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes;
+$url = 'http://download.cpuid.com/hwmonitor/hwmonitor_1.30.exe';
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = $installerType
+  url           = $url
+  url64bit      = $url
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+  softwareName  = 'hwmonitor*'
+  checksum      = '086E5B73582BB493E1B71CCCEF536865E6DFE9FC18468AC38BDFE7BE0D4486D4'
+  checksumType  = 'sha256'
+};
+
+Install-ChocolateyPackage @packageArgs;
