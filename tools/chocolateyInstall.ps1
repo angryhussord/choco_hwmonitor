6c2aaf0b6a95c2de7fe9ad42146cc494dfd85fa3
Patrick Hufford <pahuff@microsoft.com>
2017-10-29 07:26:55 -0700

    1.33 update

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageName = 'hwmonitor';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
 $installerType = 'EXE';
-$url = 'http://download.cpuid.com/hwmonitor/hwmonitor_1.32.exe';
+$url = 'http://download.cpuid.com/hwmonitor/hwmonitor_1.33.exe';
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,7 +14,7 @@ $packageArgs = @{
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
   softwareName  = 'hwmonitor*'
-  checksum      = '0877C05FDC75B0CA09587F651571B05A29E567D0A0A5C0E2592BAC6796C089A2'
+  checksum      = '5437CFAE82547ECE2C9B72D9DA8135EA67109D0942D95590C6F863D0582AD87A'
   checksumType  = 'sha256'
 };
 
