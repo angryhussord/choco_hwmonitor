5900a5b08b97c6e423f5015a678e60ee0fd43602
Ionred <bryanchaffin@bryanchaffin.com>
2019-09-10 14:09:35 -0500

    fix 64 / remap source git

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -10,12 +10,12 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = $installerType
   url           = $url
-  url64bit      = $url
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
   softwareName  = 'hwmonitor*'
   checksum      = '88BF47896738698CE3C5DB75B876283584AF9FD0F951D80CF6996547E7A93AF3'
   checksumType  = 'sha256'
 };
-
+if (Test-Path "C:\users\Public\Desktop\CPUID HWMonitor.lnk") {$KeepIcon = $True}
 Install-ChocolateyPackage @packageArgs;
+if (!$KeepIcon) {Remove-Item 'C:\Users\Public\Desktop\CPUID HWMonitor.lnk'};
