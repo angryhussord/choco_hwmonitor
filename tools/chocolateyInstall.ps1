3361b454a364a500647b3b7a54b7ed90f4580cc2
Ionred <bryanchaffin@bryanchaffin.com>
2019-09-10 14:28:08 -0500

    Update to 1.41

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageName = 'hwmonitor';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
 $installerType = 'EXE';
-$url = 'http://download.cpuid.com/hwmonitor/hwmonitor_1.40.exe';
+$url = 'http://download.cpuid.com/hwmonitor/hwmonitor_1.41.exe';
 
 $packageArgs = @{
   packageName   = $packageName
@@ -13,7 +13,7 @@ $packageArgs = @{
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
   softwareName  = 'hwmonitor*'
-  checksum      = '88BF47896738698CE3C5DB75B876283584AF9FD0F951D80CF6996547E7A93AF3'
+  checksum      = '2203E10B9277C0D74C3C12DC6808EA82F581FE01BD78A51B064D413EEA9FD149'
   checksumType  = 'sha256'
 };
 if (Test-Path "C:\users\Public\Desktop\CPUID HWMonitor.lnk") {$KeepIcon = $True}
